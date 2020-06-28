7715be5cdda2ba100708ae017e0109363f8cf416
DiruSec <gripeais@gmail.com>
2020-06-29 02:15:05 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    gitclone: {
+      fontawesome: {
+        options: {
+          repository: 'https://github.com/FortAwesome/Font-Awesome.git',
+          directory: 'tmp/fontawesome'
+        },
+      },
+      fancybox: {
+        options: {
+          repository: 'https://github.com/fancyapps/fancyBox.git',
+          directory: 'tmp/fancybox'
+        }
+      },
+      highlightjs: {
+        options: {
+          repository: 'https://github.com/highlightjs/cdn-release.git',
+          directory: 'tmp/highlightjs'
+        },
+      }
+    },
+    copy: {
+      fontawesome: {
+        expand: true,
+        cwd: 'tmp/fontawesome/fonts/',
+        src: ['**'],
+        dest: 'source/css/fonts/'
+      },
+      fontawesomecss: {
+        expand: true,
+        cwd: 'tmp/fontawesome/css/',
+        src: ['fontawesome.min.css'],
+        dest: 'source/css/fonts/'
+      },
+      fancybox: {
+        expand: true,
+        cwd: 'tmp/fancybox/source/',
+        src: ['**'],
+        dest: 'source/fancybox/'
+      },
+      highlightjs: {
+        expand: true,
+        cwd: 'tmp/highlightjs/build/',
+        src: ['highlight.min.js', 'styles/monokai.min.css'],
+        dest: 'source/highlightjs/'
+      }
+    },
+    _clean: {
+      tmp: ['tmp'],
+      fontawesome: ['source/css/fonts'],
+      fancybox: ['source/fancybox']
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.renameTask('clean', '_clean');
+
+  grunt.registerTask('highlightjs', ['gitclone:highlightjs', 'copy:highlightjs', '_clean:tmp']);
+  grunt.registerTask('fontawesome', ['gitclone:fontawesome', 'copy:fontawesome', 'copy:fontawesomecss', '_clean:tmp']);
+  grunt.registerTask('fancybox', ['gitclone:fancybox', 'copy:fancybox', '_clean:tmp']);
+  grunt.registerTask('default', ['gitclone', 'copy', '_clean:tmp']);
+  grunt.registerTask('clean', ['_clean']);
+};
